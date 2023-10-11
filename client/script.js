21cba2f89cd17a81528bd2cc680868ee11674314
TechALynch <Anthony.Lynch001@outlook.com>
2023-10-11 11:28:50 -0400

    Axios call 1

--- client/script.js
+++ client/script.js
@@ -1,25 +1,33 @@
 const btn = document.getElementById("search-btn");
 
+pants.addEventListener("click", async () => {
+    const base_Url = "http://localhost:3001/";
 
-btn.addEventListener("click", async () => {
-    const clothingApi = "http://localhost:3001/home";
-    
-    // Gets the value (char) of the input
-    const inpWord = document.getElementById("inp-word").value.toLowerCase();
-    console.log(inpWord);
+    const pantsContent = document.getElementById("pants");
 
-    try {
-        // Make the API request
-        let response = await axios.get(`http://localhost:3001/${inpWord}`);
-        const wordData = response.data[0];
-        console.log(wordData)
-        console.log(response)
-
-    // Error message
-    } catch (error) {
-        // Show an error message in a popup
-        alert(`Error: Item was not found in our database! Please re-enter a valid item!`);
-        wordName.innerHTML = ``
-        toggleButton.style.display = "none"; // Make image invisable if none exist
+    async function getAllPants() {
+        try {
+            // Make the API request
+            const response = await axios.get(`${base_Url}pants`);
+            
+            if (response.data.length > 0) {
+                const pantsInfo = response.data[0];
+                // Assuming pantsInfo is an object with properties you want to display
+                pantsContent.innerHTML = `
+                    <p>Waist Size: ${pantsInfo.waist_size}</p>
+                    <p>Length Size: ${pantsInfo.length_size}</p>
+                    <p>Color: ${pantsInfo.color}</p>
+                `;
+            } else {
+                // Show an error message if no data is returned
+                pantsContent.innerHTML = "No pants found.";
+            }
+        } catch (error) {
+            // Show an error message in a popup or log it to the console
+            console.error("Error:", error);
+            pantsContent.innerHTML = "Error: Item was not found in our database! Please re-enter a valid item!";
+        }
     }
+
+    getAllPants();
 });
