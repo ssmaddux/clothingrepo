a394020684159456e4801f5be45ac4aa8974b2e3
SageM <madduxsage@gmail.com>
2023-10-12 11:39:28 -0400

    busted cud.html'

--- client/cud.js
+++ client/cud.js
@@ -1,9 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const base_Url = "http://localhost:3001/";
-    const shirtsContent = document.getElementById("create");
+    // const base_Url = "http://localhost:3001/";
+    // const createShirtsContent = document.getElementById("create");
+    // const shirtSizeInput = document.getElementById("size")
+    // const shirtColorInput = document.getElementById("color")
+    // const shirtPriceInput = document.getElementById("price")
 
-    shirtsContent.addEventListener('click', async () => {
-        
+    // shirtsContent.addEventListener('click', async () => {
+    //     try {
+    //         const response = await axios.post(`${base_Url}shirts`);
+    //         = shirtSizeInput.innerText
+
+
+    //     } catch (error) {
+            
+    //     }
+    // })
+
+
+    const baseUrl = "http://localhost:3001/";
+    const createShirtsButton = document.getElementById("create")
+    const shirtSizeInput = document.getElementById("size").value
+    const shirtColorInput = document.getElementById("color").value
+    const shirtPriceInput = document.getElementById("price").value
+    
+    createShirtsButton.addEventListener('click', async () => {
+      try {
+        // Send a POST request to the /shirts endpoint with the shirt data.
+        const response = await axios.post(`${baseUrl}shirts`, {
+          size: shirtSizeInput,
+          color: shirtColorInput,
+          price: shirtPriceInput
+        })
+    
+        // Handle the response (e.g., show a success message).
+        console.log('Shirt created:', response.data)
+      } catch (error) {
+        // Handle any errors that occur during the POST request.
+        console.error('Error creating shirt:', error)
+      }
     })
-
-})    
+    
+   
